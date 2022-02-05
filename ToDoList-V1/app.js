4284b1e663b2a2013b88ab7eda09248ff109875e
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-02-05 16:36:22 +0530

    build(): create get request

--- ToDoList-V1/app.js
+++ ToDoList-V1/app.js
@@ -1,3 +1,22 @@
-const express = require(express)
-const bodyParser = require(bodyParser)
+const express = require("express")
+const bodyParser = require("body-parser")
 
+const app = express()
+
+const PORT = 3000
+
+app.get("/", function(req, res){
+    var today = new Date()
+
+    if(today.getDay()===6 || today.getDay() === 0){
+        res.send(`It is a holiday day. ${today}`)
+    }
+    else{
+       res.send(`It is a working day. ${today}`)
+    }
+    res.send("Hello")
+})
+
+app.listen(3000, function(){
+    console.log(`Server started and listen on ${PORT}`)
+})
